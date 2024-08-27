fc145771bde38a14dbd3eddc289b3ec8121a9c61
Au-ust <2837904488@qq.com>
2024-08-27 18:03:50 +0800

    Create test.c

---
+++ IO/简单实现封装库/test.c
@@ -0,0 +1,20 @@
+#include"mystdio.h"
+#define FILE_NAME "log.txt"
+int main() {
+	myFILE* fp = my_fopen(FILE_NAME, "w");
+	if (fp == NULL) {
+		return 1;
+	}
+	const char* str = "hello world!";
+	int cnt = 10;
+	char buffer[128];
+	while (cnt) {
+		sprintf(buffer, "%s - %d\n", str, cnt);
+		my_fwrite(fp, buffer, strlen(buffer));
+		cnt--;
+		sleep(1);
+	}
+	my_fclose(fp);
+	return 0;
+
+}
