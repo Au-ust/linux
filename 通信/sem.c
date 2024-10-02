9b58325d15160438a52715fe317bb9e81639f8c7
August <2837904488qq.com>
2024-10-03 03:04:17 +0800

    信号量的小实践

--- 通信/sem.c
+++ 通信/sem.c
@@ -10,19 +10,25 @@
 #include<sys/shm.h>
 #define PATH_NAME "./"
 #define PROJ_ID 0X6667
-#define SIZE 4097
 
 union semun{
     int val;
     struct semid_ds *buf;
 };//联合体，联合体内的成员共享同一块空间
-struct sembuf{
+/*struct sembuf{
     unsigned short sem_num;//信号量的索引
     short sem_op;//信号量操作，<0则表示P，>0则表示V,0表示等待
     short sem_flg;//操作标志,IPC_NOWAIT(非阻塞)和SEM_UNDO(系统恢复时撤销操作)
 };
-
-
+*/
+void mysemop(int num,int op,int semid){
+    struct sembuf buf;
+    buf.sem_num=num;
+    buf.sem_op=op;
+    buf.sem_flg=0;
+    semop(semid,&buf,1);
+}
+int errno;
 
 int main(){
     //创建key
@@ -32,54 +38,42 @@ int main(){
         return 1;
     }
     //创建信号量
-    int semid= semget(key,3,IPC_EXCL|IPC_CREAT|0666);
+    int semid= semget(key,2,IPC_EXCL|IPC_CREAT|0666);
     if(semid<0){
-        perror("semget");
-        return -1;
+        if(errno==17){
+            semget(key,2,0666);
+        }else{
+             perror("semget");
+             exit(0);
+            }
      }
+    printf("key==%d\nsemid==%d\n",key,semid);
     //设置信号量的值
-    union semun argv;
-    argv.val=1;
-    int result=semctl(semid,0,SETVAL,argv);
+    union semun sem;
+    sem.val=0;
+    int result=semctl(semid,0,SETVAL,sem);
     if(result<0){
         perror("semctl");
         return -1;
     }
-    //等待信号量的值为0
-    struct sembuf sop;
-    sop.sem_num=0;
-    sop.sem_op=0;
-    sop.sem_flg=0;
-    result=semop(semid,&sop,1);
+    sem.val=10;
+    result=semctl(semid,0,SETVAL,sem);
     if(result<0){
-        perror("semop");
+        perror("semctl");
         return -1;
     }
-    printf("Semaphore operation complete.\n");
-    //对信号量进行P操作
-    sop.sem_op=-1;
-    result=semop(semid,&sop,1);
-      if(result<0){
-        perror("semop");
-        return -1;
-    }
-    //进行V操作
-    sop.sem_op=1;
-    result=sempo(semid,&sop,1);
-      if(result<0){
-        perror("semop");
-        return -1;
+    //对信号量进行PV操作
+    int i=6;
+    while(i--){
+        mysemop(1,-1,semid);
+        printf("sem==%d\n",semctl(semid,1,GETVAL));//获取信号量的值
     }
     //删除信号量
-     result = semctl(semid, 0, IPC_RMID);
-    if (result == -1) {
+    result = semctl(semid, 0, IPC_RMID);
+    if (result <0) {
         perror("semctl del");
         return -1;
     }
  
-    
-    printf("key==%d\nsemid==%d\n",key,semid);
-   
-    
     return 0;
 }
