6758572bfc260b30112707d22a2a362ca4375ff0
August <2837904488qq.com>
2024-10-11 20:03:13 +0800

    好了，这下是真的实现了

--- signal/mykill.c
+++ signal/mykill.c
@@ -2,13 +2,14 @@
 #include<signal.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include<stdlib.h>
 int main(int argc,char *argv[]){
     if(argc!=3){
         perror("kill false");
         return 1;
     }
-    pid_t pid=stoi(argv[2]);
-    int sig=stoi(argv[1]);
-    kill(sig,pid);
+    pid_t pid=atoi(argv[2]);
+    int sig=atoi(argv[1]);
+    kill(pid,sig);//啊啊啊啊，一开始写反参数的顺序了，气死了
     return 0;
 }
--- signal/myprocess.c
+++ signal/myprocess.c
@@ -2,7 +2,7 @@
 #include<signal.h>
 #include<sys/types.h>
 #include<unistd.h>
-int handler(int sig){
+void handler(int sig){
     printf("get a sig form myskill,sig==%d",sig);
 }
 
@@ -11,7 +11,8 @@ int main(){
     //此处的类型为一个函数指针，在执行该语句的时候执行一个handler函数，这个函数是要自己声明的
     //格式为int handler(int sig)
     while(1){
-        printf("a new process,pid==%d",getpid());
+        printf("a process,pid==%d\n",getpid());
+        fflush(stdout);
         sleep(1);
     }
     return 0;
