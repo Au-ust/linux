d973e33ee1f3852ab534b534cbcf1d16c922164b
August <2837904488qq.com>
2024-10-21 00:42:07 +0800

    这是单独进行内存分配的代码

--- pthread/pthcon.c
+++ pthread/pthcon.c
@@ -1,21 +1,36 @@
 #include<stdio.h>
 #include<pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct ThreadData{
+    char name[20];
+    int num;
+}ThreadData;
+
 void* gopthread(void* arg){
-   while(1){
+    int cnt=5;
+    while(cnt--){
     printf("pthread is running\n");
     sleep(1);
    }
 }
 int main(){
     pthread_t tid;
+    ThreadData *td=(ThreadData*)malloc(sizeof(ThreadData));
+    strcpy(td->name,"thread-1");
+    td->num=1;
     int n=pthread_create(&tid,NULL,gopthread,(void*)"pthread 1");
     if(n!=0){
         perror("creat failed\n");
         return -1;
     }
+    n=pthread_join(tid,NULL);
+    printf("n==%d\n",n);
+    printf("maim pthread begin\n");
     if(n==0){
         printf("main thread wait success\n");
     }
-
-    return 0;
+    free(td)
+;    return 0;
 }
